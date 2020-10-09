b8fbeb520ed7cddb00d838bebc90362413f9d2f7
Hayder Jebur <hayderjabur1983@gmail.com>
2020-10-08 20:18:55 -0700

    Add redux

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-import express from "express";
-import dotenv from "dotenv";
-import colors from "colors";
-import connectDB from "./config/db.js";
-import products from "./data/data.js";
+import express from 'express';
+import dotenv from 'dotenv';
+import colors from 'colors';
+import connectDB from './config/db.js';
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+import productRoutes from './routes/productRoutes.js';
 
 dotenv.config();
 
@@ -10,15 +12,16 @@ connectDB();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+app.use('/api/products', productRoutes);
 
-app.get("/api/products", (req, res) => {
+app.use(notFound);
+app.use(errorHandler);
+
+app.get('/api/products', (req, res) => {
   res.json(products);
 });
 
-app.get("/api/products/:id", (req, res) => {
+app.get('/api/products/:id', (req, res) => {
   const product = products.find((p) => p._id === req.params.id);
   res.json(product);
 });
