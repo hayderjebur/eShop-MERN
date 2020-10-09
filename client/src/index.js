2d4236d3b544333a3298eb85a0c33d0deb66c710
Hayder Jebur <hayderjabur1983@gmail.com>
2020-10-08 20:49:22 -0700

    Add Reducers and Actions

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "./bootstrap.min.css";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import './index.css';
+import './bootstrap.min.css';
+import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </Provider>,
+  document.getElementById('root')
 );
