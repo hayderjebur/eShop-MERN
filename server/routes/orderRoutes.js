11719cfad21d053cac122fd3fc281e9c154628ab
Hayder Jebur <hayderjabur1983@gmail.com>
2021-01-28 13:55:43 -0800

    add orderAction and orderReducer

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -1,8 +1,10 @@
 import express from 'express';
 const router = express.Router();
-import { addOrderItems } from '../controllers/orderController.js';
+
 import { protect } from '../middleware/authMiddleware.js';
+import { addOrderItems, getOrderById } from '../controllers/orderController.js';
 
 router.route('/').post(protect, addOrderItems);
+router.route('/:id').get(protect, getOrderById);
 
 export default router;
