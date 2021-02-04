23435d2960ed9d3ba989c2b047477a9f90fe29bd
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-03 16:30:11 -0800

    add Paypal functionalty

--- server/routes/orderRoutes.js
+++ server/routes/orderRoutes.js
@@ -6,10 +6,12 @@ import {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  getMyOrders,
 } from '../controllers/orderController.js';
 
 router.route('/').post(protect, addOrderItems);
 router.route('/:id').get(protect, getOrderById);
 router.route('/:id/pay').put(protect, updateOrderToPaid);
+router.route('/myorders').get(protect, getMyOrders);
 
 export default router;
