8cf86c30420fc070f84c2e79c803d57ac04e8f76
Hayder Jebur <hayderjabur1983@gmail.com>
2021-02-05 09:18:41 -0800

    add Search bar

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import colors from 'colors';
 import connectDB from './config/db.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
@@ -15,6 +16,10 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
